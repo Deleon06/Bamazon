204f126c396bbd8fab266303e99f2139e472b63e
Gabriel <gabrieldeleon-ny@hotmail.com>
2018-03-11 16:26:17 -0400

    Final

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,89 +1,120 @@
-var db = require('./config/config.js');
-var products = require('./models/products.js');
+
+//var Table = require('cli-table');
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
+//attempted to hide the password, just removed it 
+//var hiddenpassword = process.env.Database_PW;
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  database: "bamazon",
+  password: "BlueB@ll$1"
+});
+
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+  }
+  show_tabel();
+});
 
 function show_tabel() {
-  db.connection.query("SELECT * FROM products", function(err, res) {
+  
+  connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
+    // tried using npm cli-table, didn't work so well here had to look for another npm package
+    // var table = new Table({
+    //   head: ['product_name', 'department_name', 'price', 'stock_quantity'],
+    //   colWidths: [100,100]
+    // });
+    // table.push(JSON.stringify(res))yy;
     console.table(res);
-   wanttopurchase(res);
+    // console.log(table.toString(res));
+    // Then prompt the customer for their choice of product, pass all the products to promptCustomerForItem
+    whatitemtobuy(res);
   });
 }
 
-function wanttopurchase(inventory) {
-  db.inquirer
+// Prompt the customer for a product ID
+function whatitemtobuy(products) {
+  // Prompts user for what they would like to purchase
+  inquirer
     .prompt([
       {
         type: "input",
         name: "choice",
-        message: "What is the ID of the item you would you like to purchase?"
-      }
+        message: "What is the ID of the item you would you like to purchase? "
+        }
     ])
     .then(function(val) {
-      var choiceId = parseInt(val.choice);
-      var product = checkInventory(choiceId, inventory);
+    
+      var item = parseInt(val.choice);
+      var product = dowehaveenough(item, products);
 
-      // If there is a product with the id the user chose, prompt the customer for a desired quantity
+    
       if (product) {
         // Pass the chosen product to promptCustomerForQuantity
-        left(product);
+        howmany(product);
       }
       else {
         // Otherwise let them know the item is not in the inventory, re-run loadProducts
-        console.log("\nThat item is not in the inventory.");
-        show_tabel ();
+        console.log("\nSorry we don't carry that item.\n");
+        show_tabel();
       }
     });
 }
 
 // Prompt the customer for a product quantity
-function left(product) {
-  db.inquirer
+function howmany(product) {
+  inquirer
     .prompt([
       {
         type: "input",
         name: "quantity",
-        message: "How many would you like?"
+        message: "How many" + " "+ product.product_name+ "'s " + "would you like?"
       }
     ])
     .then(function(val) {
-  
+      // Check if the user wants to quit the program
       var quantity = parseInt(val.quantity);
 
       // If there isn't enough of the chosen product and quantity, let the user know and re-run loadProducts
       if (quantity > product.stock_quantity) {
-        console.log("\nInsufficient quantity!");
+        console.log("\nSorry we don't have that many.\n");
         show_tabel();
       }
       else {
         // Otherwise run makePurchase, give it the product information and desired quantity to purchase
-        makePurchase(product, quantity);
+       buysomething(product, quantity);
       }
     });
 }
 
 // Purchase the desired quanity of the desired item
 function buysomething(product, quantity) {
-  db.connection.query(
+  connection.query(
     "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
     [quantity, product.item_id],
     function(err, res) {
       // Let the user know the purchase was successful, re-run loadProducts
-      console.log("\nSuccessfully purchased " + quantity + " " + product.product_name + "'s!");
+      console.log("\nYou purchased " + quantity + " " + product.product_name + "'s!\n");
       show_tabel();
     }
   );
 }
 
 // Check to see if the product the user chose exists in the inventory
-function left(choiceId, inventory) {
-  for (var i = 0; i < inventory.length; i++) {
-    if (inventory[i].item_id === choiceId) {
+function dowehaveenough(item, products) {
+  for (var i = 0; i < products.length; i++) {
+    if (products[i].item_id === item) {
       // If a matching product is found, return the product
-      return inventory[i];
-    }
-  }
-  // Otherwise return null
-  return null;
+      return products[i];
+    }  
+  }return null;
+ 
 }
-
 
